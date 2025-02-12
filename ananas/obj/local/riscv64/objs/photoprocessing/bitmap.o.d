f2020cdeec90fcb5abe71d3e1fa920c58569c0d3
Fernando Guerrero <fguerrero@ecisolutions.com>
2025-02-12 13:52:36 -0600

    Update gradle, minSdk and targetSdk

---
+++ ananas/obj/local/riscv64/objs/photoprocessing/bitmap.o.d
@@ -0,0 +1,8 @@
+./obj/local/riscv64/objs/photoprocessing/bitmap.o: jni/bitmap.c \
+  jni/mem_utils.h jni/bitmap.h jni/transform.h jni/nanojpeg.h \
+  jni/bicubic_resize.h
+jni/mem_utils.h:
+jni/bitmap.h:
+jni/transform.h:
+jni/nanojpeg.h:
+jni/bicubic_resize.h:
---
+++ ananas/obj/local/riscv64/objs/photoprocessing/transform.o.d
@@ -0,0 +1,5 @@
+./obj/local/riscv64/objs/photoprocessing/transform.o: jni/transform.c \
+  jni/bitmap.h jni/transform.h jni/mem_utils.h
+jni/bitmap.h:
+jni/transform.h:
+jni/mem_utils.h:
